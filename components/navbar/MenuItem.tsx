f446d704740ed05e7d96aa8eb5682e65fab96bea
Akin Akinbobola <akinbobola71@gmail.com>
2024-07-11 16:10:44 +0100

    Add navbar

---
+++ components/navbar/MenuItem.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import {DropdownMenuItem} from "@/components/ui/dropdown-menu";
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem = ({ onClick, label }: MenuItemProps) => {
+  return (
+    <DropdownMenuItem
+      className={"px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer"}
+      onClick={onClick}
+    >
+      {label}
+    </DropdownMenuItem>
+  );
+};
+
+export default MenuItem;
